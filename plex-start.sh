a88a182f370b9f5a7eb65526ec3177054eec0305
Shashank <107322524+shashanknimje@users.noreply.github.com>
2022-08-20 05:05:47 +0530

    Create plex-start.sh

---
+++ plex-start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# This file needs to be sourced. As in run it like so, "source ./plex-start.sh"
+
+export PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR="/var/lib/plexmediaserver/Library/Application Support"
+export PLEX_MEDIA_SERVER_HOME="/usr/lib/plexmediaserver"
+export PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS=6
+
+/usr/bin/test -d "${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}" || /bin/mkdir -p "${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}"
+
+export PLEX_MEDIA_SERVER_INFO_VENDOR=$(grep ^NAME= /etc/os-release | awk -F= "{print \$2}" | tr -d \")
+export PLEX_MEDIA_SERVER_INFO_DEVICE="PC"
+export PLEX_MEDIA_SERVER_INFO_MODEL="$(uname -m)"
+export PLEX_MEDIA_SERVER_INFO_PLATFORM_VERSION=$(grep ^VERSION= /etc/os-release | awk -F= "{print \$2}" | tr -d \")
+exec "/usr/lib/plexmediaserver/Plex Media Server"
