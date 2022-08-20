6b20907c7e3fe5bb24d396a0eb6b4eb3322a297f
Shashank <107322524+shashanknimje@users.noreply.github.com>
2022-08-20 07:26:55 +0530

    Update plex-start.sh

--- plex-start.sh
+++ plex-start.sh
@@ -8,8 +8,11 @@ export PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS=6
 
 /usr/bin/test -d "${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}" || /bin/mkdir -p "${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}"
 
-export PLEX_MEDIA_SERVER_INFO_VENDOR=$(grep ^NAME= /etc/os-release | awk -F= "{print \$2}" | tr -d \")
+export PLEX_MEDIA_SERVER_INFO_VENDOR="$(grep ^NAME= /etc/os-release | awk -F= "{print \$2}" | tr -d \")"
 export PLEX_MEDIA_SERVER_INFO_DEVICE="PC"
 export PLEX_MEDIA_SERVER_INFO_MODEL="$(uname -m)"
-export PLEX_MEDIA_SERVER_INFO_PLATFORM_VERSION=$(grep ^VERSION= /etc/os-release | awk -F= "{print \$2}" | tr -d \")
+export PLEX_MEDIA_SERVER_INFO_PLATFORM_VERSION="$(grep ^VERSION= /etc/os-release | awk -F= "{print \$2}" | tr -d \")"
 exec "/usr/lib/plexmediaserver/Plex Media Server"
+
+# This is to pass the execution to the CMD at the end parameter in the Dockerfile.
+#exec "$@"
